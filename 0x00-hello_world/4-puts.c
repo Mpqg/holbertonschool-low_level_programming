d7b2ee8b3b8fcc93b571f96f29b9a3629155bcf5
Mpqg <4534@holbertonschool.com>
2022-02-10 17:18:39 -0500

    Command puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
-int main(void)
-	{
-printf( "Programming is like building a multilingual puzzle" );
-	}
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+	// initializing strings
+	char str1[] = "Programming is like building a multilingual puzzle\n";
+
+	//writing to stdout
+	puts(str1);
+
+	return 0;
+}
