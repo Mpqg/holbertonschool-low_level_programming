baf1891362efe5a7bacac5303f417d1d596b698b
Mpqg <4534@holbertonschool.com>
2022-02-10 18:42:20 -0500

    Printing size of variablesw

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This exercise prints the size of variables needed
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+return (0);
+}
