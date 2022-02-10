3f8806b46c94406a0b0b7124c752bd83bccf34fa
Mpqg <4534@holbertonschool.com>
2022-02-10 18:10:41 -0500

    Printing a message in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-
+/**
+ * main - This exercise prints a message
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-char str1[] = "\"Programming is like building a multilingual puzzle";
-puts(str1);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
