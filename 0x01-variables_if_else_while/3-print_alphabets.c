590b3c5f8b16feb95d79c13702c3b2d9771d1963
Mpqg <4534@holbertonschool.com>
2022-02-11 15:40:09 -0500

    Alpha in LC then UC

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/**
- * main -  Prints the alphabet in lowercase, and then in uppercase, followed by a new line.
- * * Return: Always 0 (Success)
+/*
+ * main -  Prints the alphabet in lowercase, and then in uppercase
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
