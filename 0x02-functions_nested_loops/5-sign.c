2f942e484a97749459d699e0db8be1a19ae00fd7
Mpqg <4534@holbertonschool.com>
2022-02-15 18:02:35 -0500

    Check and print signs of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+m#include "main.h"
 /**
  * print_sign - prints the sign of a number
  * @n: value for validate
@@ -8,14 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		-putchar('+');
 		return (1);
 	}
 	if (n == 0)
 	{
+	_putchar(48);
 		return (0);
 	}
 	if (n < 0)
 	{
-		return (2);
+		_putchar('-');
 	}
+		return (-1);
 }
