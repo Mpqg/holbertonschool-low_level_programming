e8f1ef7bc2e9f4d63c0c90cfdae359fbcaea3f30
Mpqg <4534@holbertonschool.com>
2022-02-15 17:34:12 -0500

    Check if characcters are alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,14 +4,14 @@
  * @c: value for validate
  * Return: Always 0;
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-        if ((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
