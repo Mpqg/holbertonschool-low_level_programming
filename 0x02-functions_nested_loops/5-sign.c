fca3a09a78f9ce5fbf6c83c571f0240eeb812a80
Mpqg <4534@holbertonschool.com>
2022-02-15 17:48:44 -0500

    N according the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: value for validate
+ * Return: Always 0;
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n < 0)
+	{
+		return (2);
+	}
+}
