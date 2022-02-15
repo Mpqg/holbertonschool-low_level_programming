7e1d2f36a424e6c95da50c97093e26702ed76dd1
Mpqg <4534@holbertonschool.com>
2022-02-15 18:51:36 -0500

    Abs numbers

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,13 @@
 #include "main.h"
+
 /**
-* _abs - computes the absolute of an integer
-* @n: value for validate
-* Return: Always 0;
+* _abs - convert to absolute value
+*	@n: value
+*	Return: Always 0 (Success)
 */
 int _abs(int n)
 {
 	if (n < 0)
-	{
-	return (-n);
-	}
+		return (-n);
 	return (n);
 }
