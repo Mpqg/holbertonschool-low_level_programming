3c225303ec3444ef3e6e6ba6efe994492a8d82ef
Mpqg <4534@holbertonschool.com>
2022-02-15 20:57:18 -0500

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,17 @@
 */
 int print_last_digit(int n)
 {
+	int r;
+
 	if (n < 0)
-	{
-		return (-n % 10);
-	}
-			return (n % 10);
+	n = -n;
+
+	r = n % 10;
+
+	if (r < 0)
+	r = -r;
+
+	_putchar(r + '0');
+
+	return (r);
 }
