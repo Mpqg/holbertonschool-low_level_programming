da140c9559842a4283a79e71de0d17426bbfdab1
Mpqg <4534@holbertonschool.com>
2022-02-15 22:15:55 -0500

    adding 2 integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
-/** add - 2 integers
+/**
+* add - 2 integers
 * @n1: Integer no. 1
 * @n2: Integer no.2
 * Return: Always 0
 */
+
 int add(int n1, int n2)
 {
 	return (n1 + n2);
