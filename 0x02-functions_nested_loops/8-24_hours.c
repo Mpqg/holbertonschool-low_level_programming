55bdad1fbe00dd8a60446758bfd65a298af51a05
Mpqg <4534@holbertonschool.com>
2022-02-15 21:30:54 -0500

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* jack_bauer - print clock
+* Return: Always 0
+*/
+void jack_bauer(void)
+{
+	int hh;
+	int mm;
+
+	for (hh = 0; hh < 24; hh++)
+	{
+		for (mm = 0; mm < 60; mm++)
+		{
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(58);
+			_putchar((mm / 10) + '0');
+			-putchar((mm % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
