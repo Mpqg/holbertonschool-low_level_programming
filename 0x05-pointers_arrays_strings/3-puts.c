c4cebae4b636a510bfdf41e42fa58536d492221a
Mpqg <4534@holbertonschool.com>
2022-02-21 18:54:06 -0500

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* _puts - prints a string
-*
-* @str: string to print
-*
-* Return: Always 0
-*/
+ * _puts - prints a string
+ * @str: string
+ *
+ * Return: Always 0
+ */
 void _puts(char *str)
 {
 	int x = 0;
