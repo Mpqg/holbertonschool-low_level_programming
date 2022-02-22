733ef27065f8dd9b95192069b33a6ad0bedad26f
Mpqg <4534@holbertonschool.com>
2022-02-21 21:52:05 -0500

    Modification refactor

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,21 @@
 #include "main.h"
-
 /**
-* puts2 - prints every other character of a string
-* @str: string
-*
-* Return: Always 0
-*/
+ * puts2 - Entry point
+ *
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i))
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
