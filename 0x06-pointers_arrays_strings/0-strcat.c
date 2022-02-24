df4a0f8ebb0a0f8158dc512760da65ca2ecba9c1
Mpqg <4534@holbertonschool.com>
2022-02-24 09:51:07 -0500

    concatenates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest:variable destination
+ * @src: variable
+ * Return: char*
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	c = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		c++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
+	return (dest);
+}
