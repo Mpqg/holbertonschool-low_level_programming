a4bbf570cd8ae088a76c9d93765401dc517ae560
Mpqg <4534@holbertonschool.com>
2022-03-02 12:34:40 -0500

    print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string follow by a new line
+ *
+ * @s: string to be printed
+ * Return: Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+}
