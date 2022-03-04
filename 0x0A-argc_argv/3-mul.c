be1332e87c79679b7fc41b53101ff9cec479107d
Mpqg <4534@holbertonschool.com>
2022-03-04 15:53:25 -0500

    return multip

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - return the multiplication of 2 intergers
+ *
+ * @argc: number of imputs
+ * @argv: array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1 || argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+	{
+		i = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", i);
+		return (0);
+	}
+}
