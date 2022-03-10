826fe4d182c30df00eea7482ab1e477788c6dddd
Mpqg <4534@holbertonschool.com>
2022-03-10 15:23:18 -0500

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,12 +7,12 @@
  * Return: Always 0
  */
 
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
 	int *i;
 
 	i = malloc(b);
 	if (i == NULL)
 		exit(98);
-	return ((void *)i);
+	return (i);
 }
