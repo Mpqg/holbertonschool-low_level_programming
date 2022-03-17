c6672b26365eebe555055ec156bd632233ff1908
Mpqg <4534@holbertonschool.com>
2022-03-17 18:04:38 -0500

    operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Sum two numbers
+ * 
+ * @a: number
+ * @b: number
+ * 
+ * Return: Always 0
+ */
+
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+
+/**
+ * op_add - Sub two numbers
+ * 
+ * @a: number
+ * @b: number
+ * 
+ * Return: Always 0
+ */
+
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+
+/**
+ * op_add - Mult two numbers
+ * 
+ * @a: number
+ * @b: number
+ * 
+ * Return: Always 0
+ */
+
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+
+/**
+ * op_add - Div two numbers
+ * 
+ * @a: number
+ * @b: number
+ * 
+ * Return: Always 0
+ */
+
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+
+/**
+ * op_add - Mod two numbers
+ * 
+ * @a: number
+ * @b: number
+ * 
+ * Return: Always 0
+ */
+
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
