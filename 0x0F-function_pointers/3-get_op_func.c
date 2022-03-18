36239eb3278b7b4434005a175ae64b4b2c649ad6
Mpqg <4534@holbertonschool.com>
2022-03-17 22:54:56 -0500

    trying again

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,11 +19,12 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i;
 
-	for (i = 0; i < 6; i++)
+	for (i = 0; ops[i].op; i++)
 	{
 		if (*(ops[i].op) == *s)
 		{
 			return (ops[i].f);
 		}
 	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,24 +9,40 @@
  *
  * Return: Always 0
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (!get_op_func(argv[2]) || (argv[2][1] != NULL))
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
