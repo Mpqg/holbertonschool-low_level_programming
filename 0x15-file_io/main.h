91f75c2e94c02eb3e11749fd38c35b3904435215
Mpqg <4534@holbertonschool.com>
2022-04-05 22:15:12 -0500

    fixing code

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,6 +10,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 #endif
