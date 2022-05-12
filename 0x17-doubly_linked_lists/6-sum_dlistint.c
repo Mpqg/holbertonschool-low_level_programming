14a06ef2aa31f99a5ca1fbd8be4cc2b4cfd4e5d7
Mpqg <4534@holbertonschool.com>
2022-05-12 12:01:26 -0500

    returns the sum of all the data (n) of a dlistint_t

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Adds the value of all of the nodes
+ * @head: Address of the first node in the list
+ * Return: The sum of all values
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	for (; head != NULL; head = head->next)
+		sum += head->n;
+	return (sum);
+}
